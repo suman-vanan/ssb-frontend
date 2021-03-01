a141a17ce4cd7b17847d90fa2d083cae5ed6b87c
Suman Vanan <suman.vanan@live.com>
2021-03-01 11:00:47 +0800

    Added shebang to bash script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 echo "Injecting secrets..."
 echo "Updating iOS secret"
 echo $IOS_SECRET | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/ios/SpiceSpiceBaby/AppCenter-Config.plist"
