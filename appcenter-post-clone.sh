c64b0ea23e9276342d70ea9bc4eeaaabdeda813e
Suman Vanan <suman.vanan@live.com>
2021-03-01 11:29:45 +0800

    Removed AppCenter scripts

--- appcenter-post-clone.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Injecting secrets..."
-echo "Updating iOS secret"
-echo $IOS_SECRET | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/ios/SpiceSpiceBaby/AppCenter-Config.plist"
-echo "Finished injecting secrets."
